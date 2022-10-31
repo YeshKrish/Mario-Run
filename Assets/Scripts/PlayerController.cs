918deb5027101cd94af18fc55175586b55cbebcc
YeshKrish <yeshwinkrishna@gmail.com>
2022-11-01 00:21:30 +0530

    3D character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,25 +13,31 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody _rb;
 
+    float _smoothTurnTime = 0.1f;
+    float _smoothVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();       
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _rb.velocity = new Vector3(0f, 5f, 0f);
-        }
         _hInput = Input.GetAxis("Horizontal");
         _vInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(_hInput, 0, _vInput).normalized;
-        float targetAngle = Mathf.Atan2(_hInput, _vInput) * Mathf.Rad2Deg;
+        Vector3 direction = new Vector3(_hInput, 0f, _vInput).normalized;
 
-        gameObject.transform.rotation = Quaternion.Euler(targetAngle, 0f, 0f); 
-        controller.Move(direction * speed * Time.deltaTime);
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _smoothVelocity, _smoothTurnTime);
+            gameObject.transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
     }
 }
