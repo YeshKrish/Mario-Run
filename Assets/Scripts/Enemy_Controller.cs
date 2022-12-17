dcc1dc73422939f19ccd724c29e2de01bab38bdb
YeshKrish <yeshwinkrishna@gmail.com>
2022-12-17 18:47:34 +0530

    misc

--- Assets/Scripts/Enemy_Controller.cs
+++ Assets/Scripts/Enemy_Controller.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Enemy_Controller : MonoBehaviour
 {
     [SerializeField] GameObject burst;
+
     private void Update()
-    {
+    {  
+
         if(this.transform.position.y < -2.5f)
         {
             Instantiate(burst, transform.position, transform.rotation);
             Destroy(this.gameObject);
         }
     }
-
 }
     
     
